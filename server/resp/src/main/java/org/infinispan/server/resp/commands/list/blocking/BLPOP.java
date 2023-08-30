7e047bc87ea8a2d2df17b3326316359882980bde
rigazilla <vrigamon@redhat.com>
2023-08-30 19:10:39 +0200

    ISPN-15148 BLPOP redis command

---
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/list/blocking/BLPOP.java
@@ -0,0 +1,172 @@
+package org.infinispan.server.resp.commands.list.blocking;
+
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+import org.infinispan.AdvancedCache;
+import org.infinispan.commons.dataconversion.MediaType;
+import org.infinispan.commons.logging.LogFactory;
+import org.infinispan.commons.marshall.WrappedByteArray;
+import org.infinispan.commons.util.concurrent.CompletableFutures;
+import org.infinispan.encoding.DataConversion;
+import org.infinispan.multimap.impl.EmbeddedMultimapListCache;
+import org.infinispan.multimap.impl.ListBucket;
+import org.infinispan.notifications.Listener;
+import org.infinispan.notifications.cachelistener.annotation.CacheEntryCreated;
+import org.infinispan.notifications.cachelistener.event.CacheEntryEvent;
+import org.infinispan.notifications.cachelistener.filter.CacheEventConverter;
+import org.infinispan.server.resp.Consumers;
+import org.infinispan.server.resp.Resp3Handler;
+import org.infinispan.server.resp.RespCommand;
+import org.infinispan.server.resp.RespErrorUtil;
+import org.infinispan.server.resp.RespRequestHandler;
+import org.infinispan.server.resp.commands.ArgumentUtils;
+import org.infinispan.server.resp.commands.Resp3Command;
+import org.infinispan.server.resp.filter.EventListenerKeysFilter;
+import org.infinispan.server.resp.logging.Log;
+
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandlerContext;
+
+/**
+ * @link https://redis.io/commands/blpop/
+ * @since 15.0
+ */
+public class BLPOP extends RespCommand implements Resp3Command {
+   private static final Log log = LogFactory.getLog(MethodHandles.lookup().lookupClass(), Log.class);
+
+   public BLPOP() {
+      super(-3, 1, -2, 1);
+   }
+
+   @Override
+   public CompletionStage<RespRequestHandler> perform(Resp3Handler handler,
+         ChannelHandlerContext ctx,
+         List<byte[]> arguments) {
+      EmbeddedMultimapListCache<byte[], byte[]> listMultimap = handler.getListMultimap();
+      var lastKeyIdx = arguments.size() - 1;
+      var filterKeys = arguments.subList(0, lastKeyIdx);
+      // Using last arg as timeout if it can be a double
+      var argTimeout = ArgumentUtils.toDouble(arguments.get(lastKeyIdx));
+      if (argTimeout < 0) {
+         RespErrorUtil.mustBePositive(handler.allocator());
+         return handler.myStage();
+      }
+      long timeout = (long) (argTimeout * Duration.ofSeconds(1).toMillis());
+      // If all the keys are empty or null, create a listener for each key
+      // otherwise return the left value of the first non empty list
+      var pollStage = pollAllKeys(listMultimap, arguments);
+      // If no value returned, we need subscribers
+      return pollStage.thenCompose(v -> {
+         // addSubscriber call can rise exception that needs to be reported
+         // as error
+         try {
+            return handler.stageToReturn(
+                  (v != null && !v.isEmpty())
+                        ? CompletableFuture.completedFuture(v)
+                        : addSubscriber(listMultimap, filterKeys, timeout, handler, ctx),
+                  ctx, Consumers.COLLECTION_BULK_BICONSUMER);
+         } catch (Exception ex) {
+            RespErrorUtil.customError(ex.toString(), handler.allocator());
+            return handler.myStage();
+         }
+      });
+   }
+
+   CompletionStage<Collection<byte[]>> pollKeyValue(EmbeddedMultimapListCache<byte[], byte[]> mmList, byte[] key) {
+      return mmList.pollFirst(key, 1)
+            .thenApply((v) -> (v == null || v.isEmpty())
+                  ? null
+                  : Arrays.asList(key, v.iterator().next()));
+   }
+
+   CompletionStage<Collection<byte[]>> addSubscriber(EmbeddedMultimapListCache<byte[], byte[]> listMultimap,
+         List<byte[]> filterKeys, long timeout, Resp3Handler handler,
+         ChannelHandlerContext ctx) {
+      if (log.isTraceEnabled()) {
+         log.tracef("Subscriber for keys: " +
+               filterKeys.toString());
+      }
+      AdvancedCache<Object, Object> cache = handler.cache().withMediaType(MediaType.APPLICATION_OCTET_STREAM, null);
+      DataConversion vc = cache.getValueDataConversion();
+      PubSubListener pubSubListener = new PubSubListener(ctx.channel());
+      EventListenerKeysFilter filter = new EventListenerKeysFilter(filterKeys.toArray(byte[][]::new));
+      CacheEventConverter<Object, Object, Object> converter = (key, oldValue, oldMetadata, newValue, newMetadata,
+            eventType) -> vc.fromStorage(newValue);
+      var listnerStage = cache.addListenerAsync(pubSubListener, filter, converter);
+
+      // start a timer if needed
+      final ScheduledFuture<?> scheduledTimer = (timeout > 0) ? ctx.channel().eventLoop().schedule(() -> {
+         pubSubListener.getFuture().complete(null);
+      }, timeout, TimeUnit.MILLISECONDS) : null;
+      // Listener can lose events during its install, so we need to poll again
+      // and if we get values complete the listener future.
+      listnerStage.thenCompose(v -> pollAllKeys(listMultimap, filterKeys)).thenAccept(v -> {
+         if (v != null) {
+            pubSubListener.getFuture().complete(v);
+            if (scheduledTimer != null) {
+               scheduledTimer.cancel(true);
+            }
+         }
+      });
+      // Uninstall the listener when got values
+      return pubSubListener.getFuture().thenApply(v -> {
+         if (scheduledTimer != null) {
+            scheduledTimer.cancel(true);
+         }
+         handler.cache().removeListener(pubSubListener);
+         return v;
+      });
+   }
+
+   private CompletionStage<Collection<byte[]>> pollAllKeys(EmbeddedMultimapListCache<byte[], byte[]> listMultimap,
+         List<byte[]> filterKeys) {
+      var pollStage = pollKeyValue(listMultimap, filterKeys.get(0));
+      for (int i = 1; i < filterKeys.size(); ++i) {
+         var keyChannel = filterKeys.get(i);
+         pollStage = pollStage.thenCompose(
+               v -> (v == null || v.isEmpty())
+                     ? pollKeyValue(listMultimap, keyChannel)
+                     : CompletableFuture.completedFuture(v));
+      }
+      return pollStage;
+   }
+
+   @Listener(clustered = true)
+   public static class PubSubListener {
+      private final Channel channel;
+      private final CompletableFuture<Collection<byte[]>> future = new CompletableFuture<>();
+
+      public CompletableFuture<Collection<byte[]>> getFuture() {
+         return future;
+      }
+
+      public PubSubListener(Channel channel) {
+         this.channel = channel;
+      }
+
+      @CacheEntryCreated
+      public CompletionStage<Void> onEvent(CacheEntryEvent<Object, Object> entryEvent) {
+         if (entryEvent.getValue() instanceof ListBucket) {
+            byte[] key = unwrapKey(entryEvent.getKey());
+            var bucket = (ListBucket<byte[]>) entryEvent.getValue();
+            byte[] value = bucket == null ? null : bucket.toDeque().peek();
+            future.complete(Arrays.asList(key, value));
+         }
+         return CompletableFutures.completedNull();
+      }
+
+      private byte[] unwrapKey(Object key) {
+         return key instanceof WrappedByteArray
+               ? ((WrappedByteArray) key).getBytes()
+               : (byte[]) key;
+      }
+   }
+}
---
+++ server/resp/src/test/java/org/infinispan/server/resp/RespBlockingCommandsTest.java
@@ -0,0 +1,150 @@
+package org.infinispan.server.resp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.infinispan.server.resp.test.RespTestingUtil.createClient;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+import org.infinispan.configuration.cache.CacheMode;
+import org.infinispan.configuration.cache.ConfigurationBuilder;
+import org.infinispan.notifications.cachelistener.CacheNotifier;
+import org.infinispan.notifications.cachelistener.CacheNotifierImpl;
+import org.infinispan.server.resp.commands.list.blocking.BLPOP;
+import org.infinispan.server.resp.test.RespTestingUtil;
+import org.infinispan.test.TestingUtil;
+import org.testng.annotations.Factory;
+import org.testng.annotations.Test;
+
+import io.lettuce.core.KeyValue;
+import io.lettuce.core.RedisCommandExecutionException;
+import io.lettuce.core.RedisFuture;
+import io.lettuce.core.api.async.RedisAsyncCommands;
+import io.lettuce.core.api.sync.RedisCommands;
+
+/**
+ * Class for blocking commands tests.
+ *
+ * @author Vittorio Rigamonti
+ * @since 15.0
+ */
+@Test(groups = "functional", testName = "server.resp.RespBlockingCommandsTest")
+public class RespBlockingCommandsTest extends SingleNodeRespBaseTest {
+
+   private CacheMode cacheMode = CacheMode.LOCAL;
+   private boolean simpleCache;
+
+   @Factory
+   public Object[] factory() {
+      return new Object[] {
+            new RespBlockingCommandsTest(),
+            new RespBlockingCommandsTest().simpleCache()
+      };
+   }
+
+   RespBlockingCommandsTest simpleCache() {
+      this.cacheMode = CacheMode.LOCAL;
+      this.simpleCache = true;
+      return this;
+   }
+
+   @Override
+   protected String parameters() {
+      return "[simpleCache=" + simpleCache + ", cacheMode=" + cacheMode + "]";
+   }
+
+   @Override
+   protected void amendConfiguration(ConfigurationBuilder configurationBuilder) {
+      if (simpleCache) {
+         configurationBuilder.clustering().cacheMode(CacheMode.LOCAL).simpleCache(true);
+      } else {
+         configurationBuilder.clustering().cacheMode(cacheMode);
+      }
+   }
+
+   private CompletableFuture<KeyValue<String, String>> registerBLPOPListener(RedisAsyncCommands<String, String> redis, long timeout, String ... keys) {
+      RedisFuture<KeyValue<String, String>> rf = redis.blpop(timeout, keys);
+      CacheNotifierImpl<?, ?> cni = (CacheNotifierImpl<?, ?>) TestingUtil.extractComponent(cache, CacheNotifier.class);
+      // If there's a listener ok otherwise
+      // if rf is done an error during listener registration has happend
+      // no need to wait anymore. test will fail
+      eventually(() -> cni.getListeners().stream().anyMatch(l -> l instanceof BLPOP.PubSubListener) || rf.isDone());
+      return rf.toCompletableFuture();
+   }
+
+   @Test
+   void testBlpopAsync() throws InterruptedException, ExecutionException, TimeoutException {
+      RedisCommands<String, String> redis = redisConnection.sync();
+      var client = createClient(30000, server.getPort());
+      RedisAsyncCommands<String, String> redis1 = client.connect().async();
+      redis.lpush("keyY", "firstY");
+      redis.rpush("key1", "first", "second", "third");
+      try {
+      var cf = registerBLPOPListener(redis1, 0, "keyZ");
+      // Ensure lpush is after blpop
+         redis.lpush("keyZ", "firstZ");
+         var response = cf.get(10, TimeUnit.SECONDS);
+         assertThat(response.getKey()).isEqualTo("keyZ");
+         assertThat(response.getValue()).isEqualTo("firstZ");
+      } finally {
+         RespTestingUtil.killClient(client);
+      }
+   }
+
+   @Test
+   public void testBlpop() throws InterruptedException, ExecutionException, TimeoutException {
+      RedisCommands<String, String> redis = redisConnection.sync();
+      var client = createClient(30000, server.getPort());
+      RedisAsyncCommands<String, String> redisBlock = client.connect().async();
+      try {
+      var cf = registerBLPOPListener(redisBlock, 0, "keyZ");
+         redis.lpush("keyZ", "firstZ");
+         var res = cf.get(10, TimeUnit.SECONDS);
+         assertThat(res.getKey()).isEqualTo("keyZ");
+         assertThat(res.getValue()).isEqualTo("firstZ");
+
+         redis.rpush("key1", "first", "second", "third");
+         res = redis.blpop(0, "key1");
+         assertThat(res.getKey()).isEqualTo("key1");
+         assertThat(res.getValue()).isEqualTo("first");
+         assertThat(redis.lrange("key1", 0, -1))
+               .containsExactlyInAnyOrder("second", "third");
+         res = redis.blpop(0, "key2", "key1");
+         assertThat(res.getKey()).isEqualTo("key1");
+         assertThat(res.getValue()).isEqualTo("second");
+      } finally {
+         RespTestingUtil.killClient(client);
+      }
+   }
+
+   @Test
+   public void testBlpopTimeout() throws InterruptedException, ExecutionException {
+      RedisCommands<String, String> redis = redisConnection.sync();
+      var client = createClient(30000, server.getPort());
+      RedisAsyncCommands<String, String> redisAsync = client.connect().async();
+
+      redis.rpush("key1", "first", "second", "third");
+
+      assertThatThrownBy(() -> redisAsync.blpop(-1, "keyZ").get(10, TimeUnit.SECONDS))
+            .cause()
+            .isInstanceOf(RedisCommandExecutionException.class)
+            .hasMessageContaining("ERR value is out of range, must be positive");
+      var res = redisAsync.blpop(1, "keyZ");
+      // Ensure blpop is expired
+      eventually(res::isDone);
+      redis.lpush("keyZ", "firstZ");
+      assertThat(res.get()).isNull();
+
+      try {
+      var cf = registerBLPOPListener(redisAsync, 0, "keyY");
+         redis.lpush("keyY", "valueY");
+         assertThat(cf.get().getKey()).isEqualTo("keyY");
+         assertThat(cf.get().getValue()).isEqualTo("valueY");
+      } finally {
+         RespTestingUtil.killClient(client);
+      }
+   }
+}
