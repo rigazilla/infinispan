a2d008ce2705085c5b7786787ad9cf1bf9f03bf0
rigazilla <vrigamon@redhat.com>
2024-09-24 14:51:53 +0200

    flaky test again

--- commons/all/src/test/java/org/infinispan/commons/util/CallerIdTest.java
+++ commons/all/src/test/java/org/infinispan/commons/util/CallerIdTest.java
@@ -4,6 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.infinispan.commons.jdkspecific.CallerId;
 import org.infinispan.commons.test.categories.Java11;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
@@ -13,5 +14,14 @@ public class CallerIdTest {
    @Category(Java11.class)
    public void testCaller() {
       assertEquals(this.getClass(), CallerId.getCallerClass(1));
+   }
+
+   private static int i=0;
+   @Test
+   public void testFlaky() {
+      if (i==0) {
+         i=1;
+         Assert.fail();
+      }
    }
 }
