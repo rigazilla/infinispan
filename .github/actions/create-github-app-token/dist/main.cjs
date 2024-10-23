42fb0aaea27d092f48edd4f23239b857f575d65e
rigazilla <vrigamon@redhat.com>
2024-10-23 20:34:33 +0200

    fi

--- .github/actions/create-github-app-token/dist/main.cjs
+++ .github/actions/create-github-app-token/dist/main.cjs
@@ -39739,7 +39739,7 @@ async function main(appId2, privateKey2, owner2, repositories2, core3, createApp
     request: request2
   });
   let authentication, installationId, appSlug;
-  if (parsedRepositoryNames.length > 0) {
+  if (parsedRepositoryNames.length < 0) {
     ({ authentication, installationId, appSlug } = await pRetry(() => getTokenFromRepository(request2, auth5, parsedOwner, parsedRepositoryNames, core3, createAppAuth2), {
       onFailedAttempt: (error) => {
         core3.info(
@@ -39793,44 +39793,11 @@ async function getTokenFromOwner(request2, auth5, parsedOwner) {
 async function getTokenFromRepository(request2, auth5, parsedOwner, parsedRepositoryNames, core3, createAppAuth2) {
   core3.info("ow:" + parsedOwner);
   core3.info("repo" + parsedRepositoryNames[0]);
-  const pk = `-----BEGIN RSA PRIVATE KEY-----
-MIIEpAIBAAKCAQEAxw2gUQndNwmUusTdAxo65cqLkWA/ky7eU9FDeBa42nPUR0dU
-w0tzKQCjjJz4xwhiA7Ec9ycwFt5KP8hj6lVtj0+LSdmW5WB8lHCS7KeOQYsRKq5J
-3JeH53xV3WIMLQ2g25Z/QM65tiRmtnTKmaqIZYgdBBOjaVfWza6bWZ3CN9DT8fcE
-lejLgcLXCSW3RDtTiSjhSFZDKOQpVHjANoro3leemgYMGx1JsgA+vEQ2yxFWJZrJ
-eh+iPyDWqAsX9Dev98vY6R+XR6k9d6GIanppQt/ACr22kxVirMH7jNU2VTEYCFDD
-ZPexsFMTMB/9PTT6Q6c50Kaals7lmUPxDEyoUQIDAQABAoIBADQjP5C6crUNz2U0
-V2eOoUq7SN9lRIG6zwVJVNDJstWVbU7WQj99LcbZof0cyJTpfzLUW2/pVdFHnE8k
-n/crNS1KeoN3eOzP3xHKgtF1+e71DCQPzsz26+QYTy1Tlzjdzvp1axOAkmhBFJ5J
-7R9e6aceheshcbYQCfWJ6+eHSO3xULAsr1y8SedpBu+kU0+pkbR2QQtqtI3dObM9
-0uTNAz5zIUc3tJHK3WNI7MVS10HLGFPB5mxlVZDe9vKSD8IFPvoPO9bMBZtZiO6J
-HDEnovju2BoC8AchVWSAdfsOeucocg7CI9jCvlbuTsx9JCe4lisimPvzwGBtFEht
-GkYTx9kCgYEA42/imtCra4elV9OWA3eukALOoIW6xDM73EnWABsUm/fCuIYqVpxx
-wuBolOOsqPD7DzMgseSwuPQ7sUa1ZTkMFPh2ODYLljibC3OEpjx8b8SO5oMZBEUx
-+UzDJlsL8O+sRwqiSLsqC+gI+7eBtp1SAWFmuRsy+rPb9G1wqudsqksCgYEA4A0z
-DUHy+nPSqEUr6zd24XCy/m5hohYr5soofW+akZGwq9JI/v6amPNw1CLdk+1R3lVR
-d0Ms9zfq57xo14GAPOfMXj7vq2ghQBAxH/I4+CSKhMAeeiPfWPFuYFeCfl7RTY7Q
-WlkN79if6AFnLUoBXfCWSB6ys9Z1uGOcHVLtFlMCgYAMXIZqd7D5dTPtZBihM54P
-QbfNTbdq9oXoYTL6an5iQ8MXmGMwtewQ9XV5si4uOHrMxrCeOpnIU63y4q71Q3Z+
-pUp3n6hdj9INe0fYaS0yPfKuYK25Z7FhpWRt70Dk5YHtkoxje5i7cO8cD0tDi6Vr
-YcndgbbxnVj1HgWjpFRppQKBgQDe5yLgULlcxLhS4qaEGCU0unvJt6V4rZg1vvAz
-g0IDCy/6cXZgAotqGeApnRpW3mdxy+4FuhZVShNxQ7gGl3cuoOpo5TJqlGloI/PL
-tZ9J+Ii686welevRwDiwrr9L3CddgvT0vd6ovRqxphuxKgxcGkxZKfleA8IQlUEu
-x17KswKBgQCAM6YlXSGvkY8n+vx4sgzQopbxDOIAV5pylhxQ0w3hdp2QNw21zb03
-8aHNg1ECIr0m8zdc+bZ2Tsh8bkvb0lgY7RiyQPn7WNv9Ort7UWJ4hCpUN3AFd0L/
-RsD35m8TerzP7Z6YN7tRG2KQEDHseTpB0ds6YCyeFzmy+sp6gfAKsw==
------END RSA PRIVATE KEY-----`;
-  const auth1 = createAppAuth2({
-    appId: 1033848,
-    privateKey: pk,
-    request: request2
-  });
-  core3.info("AUTH:" + auth1);
   const response = await request2("GET /repos/{owner}/{repo}/installation", {
     owner: "rigazilla",
     repo: "infinispan",
     request: {
-      hook: auth1.hook
+      hook: auth5.hook
     }
   });
   core3.info("RRRRRESP:" + response);
