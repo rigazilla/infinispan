d34e2cc076135f0ac5b11fd55845eb785a09fbdf
rigazilla <vrigamon@redhat.com>
2024-10-24 18:01:11 +0200

    fix

--- .github/actions/create-app-token/index.js
+++ .github/actions/create-app-token/index.js
@@ -2,44 +2,44 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { App } = require('@octokit/app');
 
-// try {
-  // `who-to-greet` input defined in action metadata file
-  const privateKey = core.getInput('private-key');
-  console.log(`Hello ${privateKey}!`);
+main()
 
-//   const auth = createAppAuth({
-//    appId: APP_ID,
-//    privateKey: privateKey,
-//    request,
-//  });
+export async function main(privateKey) {
+   try {
+      // `who-to-greet` input defined in action metadata file
+      const privateKey = core.getInput('private-key');
+      console.log(`Hello ${privateKey}!`);
 
-//  console.log(auth);
-//  const install = await request("GET /repos/{owner}/{repo}/installation", {
-//    owner: "rigazilla",
-//    repo: "infinispan",
-//    request: {
-//      hook: auth.hook,
-//    },
-//  });
-main(privateKey);
+    //   const auth = createAppAuth({
+    //    appId: APP_ID,
+    //    privateKey: privateKey,
+    //    request,
+    //  });
 
-async function main(privateKey) {
-   try{
-   const app = new App({
-      appId: APP_ID,
-      privateKey: privateKey,
-    });
+    //  console.log(auth);
+    //  const install = await request("GET /repos/{owner}/{repo}/installation", {
+    //    owner: "rigazilla",
+    //    repo: "infinispan",
+    //    request: {
+    //      hook: auth.hook,
+    //    },
+    //  });
 
-     const octokit = await app.getInstallationOctokit(56304673);
-     console.log(octokit);
+     const app = new App({
+       appId: APP_ID,
+       privateKey: privateKey,
+     });
 
-     const time = (new Date()).toTimeString();
-     core.setOutput("token", time);
-     // Get the JSON webhook payload for the event that triggered the workflow
-     const payload = JSON.stringify(github.context.payload, undefined, 2)
-     console.log(`The event payload: ${payload}`);
-   } catch (error) {
-     core.setFailed(error.message);
-   }
+      const octokit = await app.getInstallationOctokit(56304673);
+      console.log(octokit);
+
+      const time = (new Date()).toTimeString();
+      core.setOutput("token", time);
+      // Get the JSON webhook payload for the event that triggered the workflow
+      const payload = JSON.stringify(github.context.payload, undefined, 2)
+      console.log(`The event payload: ${payload}`);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
 
 }
