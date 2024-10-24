18f06fd73e59aeeccc0ca1955f2ebaab06a44560
rigazilla <vrigamon@redhat.com>
2024-10-24 17:53:38 +0200

    fix

--- .github/actions/create-app-token/index.js
+++ .github/actions/create-app-token/index.js
@@ -2,7 +2,7 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const { App } = require('@octokit/app');
 
-try {
+// try {
   // `who-to-greet` input defined in action metadata file
   const privateKey = core.getInput('private-key');
   console.log(`Hello ${privateKey}!`);
@@ -21,21 +21,25 @@ try {
 //      hook: auth.hook,
 //    },
 //  });
+main(privateKey);
 
- const app = new App({
-   appId: APP_ID,
-   privateKey: privateKey,
- });
+async function main(privateKey) {
+   try{
+   const app = new App({
+      appId: APP_ID,
+      privateKey: privateKey,
+    });
 
-  const octokit = await app.getInstallationOctokit(56304673);
-  console.log(octokit);
+     const octokit = await app.getInstallationOctokit(56304673);
+     console.log(octokit);
 
-  const time = (new Date()).toTimeString();
-  core.setOutput("token", time);
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-} catch (error) {
-  core.setFailed(error.message);
+     const time = (new Date()).toTimeString();
+     core.setOutput("token", time);
+     // Get the JSON webhook payload for the event that triggered the workflow
+     const payload = JSON.stringify(github.context.payload, undefined, 2)
+     console.log(`The event payload: ${payload}`);
+   } catch (error) {
+     core.setFailed(error.message);
+   }
+
 }
-
