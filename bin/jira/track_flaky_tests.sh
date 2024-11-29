c9c6eae856233354cc25ccfbe9b67c3c2ca79e2b
rigazilla <vrigamon@redhat.com>
2024-11-29 15:23:04 +0100

    check error

--- bin/jira/track_flaky_tests.sh
+++ bin/jira/track_flaky_tests.sh
@@ -32,7 +32,7 @@ for TEST in "${TESTS[@]}"; do
 
     # Search issues for existing github issue
       ISSUES="$(gh search issues \"${SUMMARY}\" in:title --json number)"
-      if [ "${ISSUES}" -eq "" ]; then
+      if [[ "${ISSUES}" == "" ]]; then
          echo Error with gh search. Maybe rate limits reached?
          gh api rate_limit
          exit 0
