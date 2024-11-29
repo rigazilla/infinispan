3caeabbea6865230d2188e6e6b42360a14cf9420
rigazilla <vrigamon@redhat.com>
2024-11-29 18:34:53 +0100

    fix

--- bin/gh/track_flaky_tests.sh
+++ bin/gh/track_flaky_tests.sh
@@ -49,7 +49,7 @@ for TEST in "${TESTS[@]}"; do
       exit 1
     fi
 
-      BODY="Target Branch: ${TARGET_BRANCH}$'\n'Github Job:${GH_JOB_URL}$'\n'${STACK_TRACE}"
+      BODY=$(printf "Target Branch: %s\nGithub Job:%s$\n%s" "${TARGET_BRANCH}" "${GH_JOB_URL}" "${STACK_TRACE}")
     if [ ${TOTAL_ISSUES} == 0 ]; then
       echo "Existing issue not found, creating a new one"
       gh issue create --title "${SUMMARY}" --body "${BODY}" --label "Flaky Test"
