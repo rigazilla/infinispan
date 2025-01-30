e15888323b48605f715105a00a98bbf2181c94d9
rigazilla <vrigamon@redhat.com>
2025-01-30 09:08:50 +0100

    [#13884] Adding ehcache

--- server/resp/src/main/java/org/infinispan/server/resp/commands/connection/MemoryEntrySizeUtils.java
+++ server/resp/src/main/java/org/infinispan/server/resp/commands/connection/MemoryEntrySizeUtils.java
@@ -1,23 +1,23 @@
 package org.infinispan.server.resp.commands.connection;
 
+import org.ehcache.sizeof.SizeOf;
 import org.infinispan.container.entries.CacheEntrySizeCalculator;
 import org.infinispan.container.entries.InternalCacheEntry;
 import org.infinispan.container.entries.PrimitiveEntrySizeCalculator;
-import org.openjdk.jol.info.GraphLayout;
 
 public class MemoryEntrySizeUtils {
    private static PrimitiveEntrySizeCalculator pesc = new PrimitiveEntrySizeCalculator();
    private static CacheEntrySizeCalculator<byte[], Object> cesc = new CacheEntrySizeCalculator<byte[], Object>(
          MemoryEntrySizeUtils::internalCalculateSize);
-
+   private static SizeOf  sizeof = SizeOf.newInstance();
    public static long calculateSize(byte[] key, InternalCacheEntry<byte[], Object> ice) {
       return cesc.calculateSize(key, ice);
    }
 
    static long internalCalculateSize(byte[] key, Object value) {
       try {
-         var keySize = GraphLayout.parseInstance(key).totalSize();
-         var valueSize = GraphLayout.parseInstance(value).totalSize();
+         long keySize = sizeof.deepSizeOf(key);
+         long valueSize = sizeof.deepSizeOf(value);
          return keySize + valueSize;
       } catch (Exception ex) {
          // Try an old style computation
